ff81f5945da4568c7c883b3a5a816ef2a822f324
t1r2e3x4 <t1r2e3x4@gmail.com>
2025-12-24 02:14:28 +0800

    feat(state_saver.py): finish state_saver automation by email otp

--- state_saver.py
+++ state_saver.py
@@ -1,12 +1,28 @@
 from src.dog_cat_star import DogCatStar
 from pages.login_page import LoginPage
+import os
+from dotenv import load_dotenv
+from helper.mail_otp.mail_otp import get_latest_otp_email
+load_dotenv()
+EMAIL_ADDRESS = os.getenv("EMAIL_IMAP_USERNAME")
 
 def main():
     dcs = DogCatStar()
     dcs.open_homepage()
     login_page = LoginPage(dcs._page)
-    login_page.go_to_login()
-    input("登入完成後按 Enter")
+    login_page.goto()
+    login_page.email_login(EMAIL_ADDRESS)
+    
+    retry = 0
+    while True:
+        otp_pass = get_latest_otp_email("汪喵星球")
+        if otp_pass:
+            break
+        retry += 1
+        if retry >= 5:
+            print("Failed to get OTP email after several retries.")
+            return
+        print("OTP email not found yet, retrying...")
 
     dcs._context.storage_state(path="./state_storage/state.json")
     dcs._browser.close()

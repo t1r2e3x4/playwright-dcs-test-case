743350fefcb71d156e8392550c496b3a7c2477fd
t1r2e3x4 <t1r2e3x4@gmail.com>
2025-12-24 02:13:54 +0800

    feat(login_page.py): add otp_login_function

--- pages/login_page.py
+++ pages/login_page.py
@@ -16,15 +16,20 @@ class LoginPage(BasePage):
             "div.MuiBox-root > div.MuiFormControl-root"
         )
         self.phone_input: Locator = self.page.locator("input#billing_phone")
-        
+        self.ask_login_div: Locator = self.page.locator("div:has(h6:has-text('登入享優惠')):has(button:text('立即登入'))")
         
         # email login process locators
         self.email_login_container: Locator = self.page.locator("div:has(div>h4:text('使用 Email 登入'))")
         self.email_banner: Locator = self.email_login_container.locator("p:text('Email')")
         self.email_input: Locator = self.email_login_container.locator('input[name="email"]')
-        self.email_submit_btn: Locator = self.email_login_container.locator("button:has-text('確認')")
+        self.email_submit_btn: Locator = self.email_login_container.locator("button:text('確認')")
+        
+        # otp password login process locators
+        self.otp_password_input: Locator = self.page.locator('form > input[autocomplete="one-time-code"][data-gtm-form-interact-field-id=0]')
         
     def click_email_login(self):
+        expect(self.email_btn).to_be_visible()
+        expect(self.email_btn).to_be_enabled()
         self.email_btn.click()
         
     def enter_email(self, email: str):
@@ -35,11 +40,15 @@ class LoginPage(BasePage):
         self.email_submit_btn.click()
         
     def email_login(self, email: str):
-        self.go_to_login()
+        self.goto()
         self.click_email_login()
         self.enter_email(email)
-        input("完成 Email 登入後按 Enter")
         
+    def enter_otp_password(self, otp_password:str):
+        self.otp_password_input.wait_for()
+        self.otp_password_input.fill(otp_password)
+        expect(self.ask_login_div).not_to_be_visible()
+        breakpoint()
         
     
         
